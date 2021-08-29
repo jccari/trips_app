f932cc0728e866d4b0051e3f6ff2b94a9412f7d5
jccari <jhoelccari.23@gmail.com>
2021-08-28 23:51:09 -0500

    refactor: reviews list map

--- lib/widgets/reviews/reviews_list.dart
+++ lib/widgets/reviews/reviews_list.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 import 'review.dart';
 
+const reviews = [
+  {
+    "userPhotoSrc": "assets/images/jeff.jpg",
+    "userName": "Jeff Malone",
+    "content": "Este lugar es bonito",
+    "reviewsNumber": 3,
+    "photosNumber": 4,
+  },
+  {
+    "userPhotoSrc": "assets/images/ann.jpg",
+    "userName": "Anne Martinez",
+    "content": "Este lugar es bonito",
+    "reviewsNumber": 6,
+    "photosNumber": 9,
+  },
+  {
+    "userPhotoSrc": "assets/images/mercedes.jpg",
+    "userName": "Mercedes Roque",
+    "content": "Este lugar es bonito",
+    "reviewsNumber": 2,
+    "photosNumber": 8,
+  }
+];
+
 class ReviewListWidget extends StatelessWidget {
   const ReviewListWidget({Key? key}) : super(key: key);
 
@@ -8,29 +32,15 @@ class ReviewListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        ReviewWidget(
-          userPhotoSrc: "assets/images/jeff.jpg",
-          userName: "Jeff Malone",
-          content: "Este lugar es bonito",
-          reviewsNumber: 3,
-          photosNumber: 4,
-        ),
-        ReviewWidget(
-          userPhotoSrc: "assets/images/ann.jpg",
-          userName: "Anne Martinez",
-          content: "Este lugar es bonito",
-          reviewsNumber: 6,
-          photosNumber: 9,
-        ),
-        ReviewWidget(
-          userPhotoSrc: "assets/images/mercedes.jpg",
-          userName: "Mercedes Roque",
-          content: "Este lugar es bonito",
-          reviewsNumber: 2,
-          photosNumber: 8,
-        ),
-      ],
+      children: reviews.map((item) {
+        return ReviewWidget(
+          userPhotoSrc: item["userPhotoSrc"] as String,
+          userName: item["userName"] as String,
+          content: item["content"] as String,
+          reviewsNumber: item["reviewsNumber"] as int,
+          photosNumber: item["photosNumber"] as int,
+        );
+      }).toList(),
     );
   }
 }
