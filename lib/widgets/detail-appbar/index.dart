35759faed6356560b9c2844cd79fe50392518c5b
jccari <jhoelccari.23@gmail.com>
2021-08-29 01:00:13 -0500

    add carrousel

--- lib/widgets/detail-appbar/index.dart
+++ lib/widgets/detail-appbar/index.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 import '../gradient-background/index.dart';
+import '../carrousel/carrousel.dart';
+
+const List<String> imageList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
 
 class DetailAppBarWidget extends StatelessWidget {
   final String title;
@@ -18,7 +28,7 @@ class DetailAppBarWidget extends StatelessWidget {
           children: [
             Container(
               margin: EdgeInsets.only(
-                top: 50.0,
+                top: 30.0,
                 left: 20.0,
               ),
               child: Align(
@@ -33,7 +43,7 @@ class DetailAppBarWidget extends StatelessWidget {
                 ),
               ),
             ),
-            // horizontal images list
+            CarrouselWidget(images: imageList),
           ],
         )
       ],
